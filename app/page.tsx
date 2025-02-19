9eb92b66875eacdfadd29981535c25c3f4f8e790
sanjay2738873827348 <115889973+skavva-ship-it@users.noreply.github.com>
2025-02-18 20:23:06 -0500

    fixed scrollbarbug

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="relative w-screen overflow-x-hidden">
+    <main className="relative">
       <div className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
       </div>
-      <div className="relative w-full">
+      <div className="relative">
         <Hero />
       </div>
     </main>
